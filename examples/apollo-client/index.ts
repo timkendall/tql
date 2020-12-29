6b80f912467ea6f7f4ae18f3baaa7d96fe5f401f
Tim Kendall <timkendall18@gmail.com>
2020-12-28 16:41:02 -0800

    Add small apollo-client example

---
+++ examples/apollo-client/index.ts
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+import { Result } from "../../src";
+import { query, IQuery } from "../../__tests__/starwars/starwars.api";
+
+const client = new ApolloClient({ cache: new InMemoryCache() });
+
+const example = query("Apollo Example", (t) => [
+  t.starship({ id: "foo" }, (t) => [
+    t.id(),
+    t.name(),
+    t.length({ unit: "any" }),
+    t.coordinates(),
+  ]),
+]);
+
+const apolloQuery = client.query<
+  Result<IQuery, typeof example["selectionSet"]>
+>({ query: example.toDocument() });
+
+apolloQuery.then((result) => {
+  console.log(result.data.starship.length);
+});
