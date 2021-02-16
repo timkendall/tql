fc232c65c48b8671dea83a04073e3f5c3d3f781a
Tim Kendall <timkendall18@gmail.com>
2021-02-15 16:17:50 -0800

    Add example of bug with abstract type collections

--- examples/apollo-client/index.ts
+++ examples/apollo-client/index.ts
@@ -6,12 +6,30 @@ import {
   IQuery,
   LengthUnit,
   Episode,
+  isDroid,
+  isHuman,
 } from "../../__tests__/starwars/starwars.sdk";
 
 const client = new ApolloClient({ cache: new InMemoryCache() });
 
 const example = query("Apollo Example", (t) => [
-  t.character({ id: "foo" }, (t) => [t.id(), t.friends((t) => [t.id()])]),
+  t.character({ id: "foo" }, (t) => [
+    t.id(),
+    t.friends((t) => [
+      t.__typename(),
+      t.id(),
+
+      t.on("Droid", (t) => [t.primaryFunction()]),
+      t.on("Human", (t) => [t.homePlanet()]),
+    ]),
+  ]),
+
+  t.search({ text: "" }, (t) => [
+    t.__typename(),
+
+    t.on("Droid", (t) => [t.primaryFunction()]),
+    t.on("Human", (t) => [t.homePlanet()]),
+  ]),
 
   t.starship({ id: "foo" }, (t) => [
     t.id(),
@@ -29,4 +47,18 @@ const apolloQuery = client.query<
 
 apolloQuery.then((result) => {
   console.log(result.data.starship?.name);
+
+  // Ugh abstract types are giving us issues when used in collections...
+  const friends = result.data.character?.friends ?? [];
+  const search = result.data.search!;
+
+  for (const friend of search) {
+    if (friend.__typename === "Human") {
+      console.log(`Friend is a Human from "${friend.homePlanet}"!`);
+    }
+
+    if (friend.typename === "Droid") {
+      console.log(`Friend is a Droid for "${friend.primaryFunction}"!`);
+    }
+  }
 });
