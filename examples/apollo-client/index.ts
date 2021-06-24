bfbce55f069d9123210854f17acec582dab7efe3
Tim Kendall <timkendall18@gmail.com>
2021-06-24 12:43:36 -0700

    Improve apollo-client example

--- examples/apollo-client/index.ts
+++ examples/apollo-client/index.ts
@@ -1,4 +1,5 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import fetch from "node-fetch";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 import { Result } from "../../src";
 import {
@@ -8,25 +9,28 @@ import {
   Episode,
 } from "../../__tests__/starwars/starwars.sdk";
 
-const client = new ApolloClient({ cache: new InMemoryCache() });
+(async () => {
+  const client = new ApolloClient({
+    link: new HttpLink({ uri: "http://localhost:8080/graphql", fetch }),
+    cache: new InMemoryCache(),
+  });
 
-const example = query("Apollo Example", (t) => [
-  t.character({ id: "foo" }, (t) => [t.id(), t.friends((t) => [t.id()])]),
+  const example = query("ApolloExample", (t) => [
+    t.character({ id: "foo" }, (t) => [t.id(), t.friends((t) => [t.id()])]),
 
-  t.starship({ id: "foo" }, (t) => [
-    t.id(),
-    t.name(),
-    t.length({ unit: LengthUnit.FOOT }),
-    t.coordinates(),
-  ]),
+    t.starship({ id: "foo" }, (t) => [
+      t.id(),
+      t.name(),
+      t.length({ unit: LengthUnit.FOOT }),
+      t.coordinates(),
+    ]),
 
-  t.reviews({ episode: Episode.JEDI }, (t) => [t.commentary()]),
-]);
+    t.reviews({ episode: Episode.JEDI }, (t) => [t.commentary()]),
+  ]);
 
-const apolloQuery = client.query<
-  Result<IQuery, typeof example["selectionSet"]>
->({ query: example.toDocument() });
+  const result = await client.query<
+    Result<IQuery, typeof example["selectionSet"]>
+  >({ query: example.toDocument() });
 
-apolloQuery.then((result) => {
-  console.log(result.data.starship?.name);
-});
+  console.log(`Found Starship "${result.data.starship?.name}"!`);
+})();
