1b1e455748e7f91d6766d8adf7b19e8ed4e74bab
Tim Kendall <timkendall18@gmail.com>
2021-11-25 11:57:25 -0800

    Uncomment Variables tests

--- src/__tests__/Variables.test.ts
+++ src/__tests__/Variables.test.ts
@@ -1,94 +1,81 @@
-// import { buildSchema } from "graphql";
+import { buildSchema } from "graphql";
 
-// import {
-//   namedType,
-//   nonNull,
-//   field,
-//   argument,
-//   variable,
-//   selectionSet,
-//   SelectionSet,
-//   variableDefinition,
-//   operation,
-//   Selection,
-//   Field,
-//   Argument,
-//   Variable,
-// } from "../AST";
-// import { Selected } from "../Selector";
-// import { buildVariableDefinitions, Variables } from "../Variables";
+import {
+  namedType,
+  nonNull,
+  field,
+  argument,
+  variable,
+  selectionSet,
+  variableDefinition,
+} from "../AST";
+import { Selection } from "../Selection";
+import { buildVariableDefinitions } from "../Variables";
 
-// describe("Variables", () => {
-//   it("infers variable definitions", () => {
-//     const selection = new Selected("Query", [
-//       field("hello", [argument("name", variable("name"))]),
-//     ]);
+describe("Variables", () => {
+  it("builds variable definitions", () => {
+    const schema = buildSchema(
+      `
+      type Query {
+        hello(name: String!): String!
+      }
+    `,
+      { noLocation: true }
+    );
 
-//     interface Query {
-//       hello(variables: { name: string }): string;
-//     }
+    const selection = new Selection(schema, "Query", [
+      field("hello", [argument("name", variable("name"))]),
+    ]);
 
-//     const variables: Variables<Query, SelectionSet<typeof selection>> = {
-//       name: "string",
-//     };
-//   });
+    // @note we need a way to get the input type at runtime
+    const variableDefinitions = buildVariableDefinitions(
+      "query",
+      schema,
+      selectionSet(selection)
+    );
 
-//   it("builds variable definitions", () => {
-//     const selection = new Selected("Query", [
-//       field("hello", [argument("name", variable("name"))]),
-//     ]);
+    expect(variableDefinitions).toEqual([
+      variableDefinition(variable("name"), nonNull(namedType("String"))),
+    ]);
+  });
 
-//     interface Query {
-//       hello(variables: { name: string }): string;
-//     }
+  it("infers nested variable definitions", () => {
+    const schema = buildSchema(
+      `
+      type Query {
+        viewer: User!
+      }
 
-//     const schema = buildSchema(
-//       `
-//       type Query {
-//         hello(name: String!): String!
-//       }
-//     `,
-//       { noLocation: true }
-//     );
+      type User {
+        id: ID!
+        friends(limit: Int = 10): [User!]
+      }
+    `,
+      { noLocation: true }
+    );
 
-//     // @note we need a way to get the input type at runtime
-//     const variableDefinitions = buildVariableDefinitions(
-//       "query",
-//       schema,
-//       selectionSet(selection)
-//     );
+    const selection = new Selection(schema, "Query", [
+      field(
+        "viewer",
+        undefined,
+        selectionSet([
+          field(
+            "friends",
+            [argument("limit", variable("friendsLimit"))],
+            selectionSet([field("id")])
+          ),
+        ])
+      ),
+    ]);
 
-//     expect(variableDefinitions).toEqual([
-//       variableDefinition(variable("name"), nonNull(namedType("String"))),
-//     ]);
-//   });
+    const variableDefinitions = buildVariableDefinitions(
+      "query",
+      schema,
+      selectionSet(selection)
+    );
 
-//   it("infers nested variable definitions", () => {
-//     const selection = new Selected("Query", [
-//       field(
-//         "viewer",
-//         undefined,
-//         selectionSet([
-//           field(
-//             "friends",
-//             [argument("limit", variable("friendsLimit"))],
-//             selectionSet([field("id")])
-//           ),
-//         ])
-//       ),
-//     ]).toSelectionSet();
-
-//     interface Query {
-//       viewer: User;
-//     }
-
-//     interface User {
-//       id: string;
-//       friends(variables: { limit?: number }): User[];
-//     }
-
-//     const variables: Variables<Query, typeof selection> = {
-//       friendsLimit: 5,
-//     };
-//   });
-// });
+    expect(variableDefinitions).toEqual([
+      variableDefinition(variable("friendsLimit"), nonNull(namedType("Int"))),
+    ]);
+  });
+});
