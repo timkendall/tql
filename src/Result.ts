588f5eff80497e297580465834ace843ee8a43ee
Tim Kendall <timkendall18@gmail.com>
2022-01-03 16:11:27 -0800

    Add dprint command to git pre-commit hook

--- src/Result.ts
+++ src/Result.ts
@@ -1,14 +1,6 @@
 import { L, Test } from "ts-toolbelt";
 
-import type {
-  Primitive,
-  NamedType,
-  SelectionSet,
-  Selection,
-  Field,
-  InlineFragment,
-  FragmentSpread,
-} from "./AST";
+import type { Field, InlineFragment, NamedType, Selection, SelectionSet } from "./AST";
 
 // @note `Result` takes a root `Type` (TS) and `SelectionSet` (GQL) and recursively walks the
 // array of `Selection` nodes's (i.e `Field`, `InlineFragment`, or `FragmentSpread` nodes)
@@ -19,79 +11,73 @@ import type {
 export type Result<
   Schema extends Record<string, any>,
   Parent,
-  Selected extends SelectionSet<ReadonlyArray<Selection>> | undefined
+  Selected extends SelectionSet<ReadonlyArray<Selection>> | undefined,
 > =
   // Lists
-  Parent extends Array<infer T> | ReadonlyArray<infer T>
-    ? ReadonlyArray<Result<Schema, T, Selected>>
+  Parent extends Array<infer T> | ReadonlyArray<infer T> ? ReadonlyArray<Result<Schema, T, Selected>>
     : // Objects
     Parent extends Record<string, any>
-    ? Selected extends SelectionSet<ReadonlyArray<Selection>>
-      ? HasInlineFragment<Selected> extends Test.Pass
-        ? SpreadFragments<Schema, Selected>
+      ? Selected extends SelectionSet<ReadonlyArray<Selection>>
+        ? HasInlineFragment<Selected> extends Test.Pass ? SpreadFragments<Schema, Selected>
         : {
-            readonly // @todo cleanup mapped typed field name mapping
-            [F in Selected["selections"][number] as F extends Field<
+          readonly // @todo cleanup mapped typed field name mapping
+          [
+            F in Selected["selections"][number] as F extends Field<
               infer Name,
               any,
               any
-            >
-              ? Name
-              : never]: F extends Field<infer Name, any, infer SS>
-              ? Result<
-                  Schema,
-                  /* @note support parameterized fields */ Parent[Name] extends (
-                    variables: any
-                  ) => infer T
-                    ? T
-                    : Parent[Name] /*Parent[Name]*/,
-                  SS
-                >
-              : never;
-          }
+            > ? Name
+              : never
+          ]: F extends Field<infer Name, any, infer SS> ? Result<
+            Schema,
+            /* @note support parameterized fields */ Parent[Name] extends (
+              variables: any,
+            ) => infer T ? T
+              : Parent[Name], /*Parent[Name]*/
+            SS
+          >
+            : never;
+        }
       : never
     : // Scalars
-      Parent;
+    Parent;
 
 export type SpreadFragments<
   Schema extends Record<string, any>,
-  Selected extends SelectionSet<ReadonlyArray<Selection>>
+  Selected extends SelectionSet<ReadonlyArray<Selection>>,
 > = Selected["selections"][number] extends infer Selection
-  ? Selection extends InlineFragment<any, any>
-    ? SpreadFragment<
-        Schema,
-        Selection,
-        SelectionSet<L.Filter<Selected["selections"], InlineFragment<any, any>>> // @bug are we are losing inference here since `SelectionSet<[Field<'id'>]>` works?
-      >
-    : never
+  ? Selection extends InlineFragment<any, any> ? SpreadFragment<
+    Schema,
+    Selection,
+    SelectionSet<L.Filter<Selected["selections"], InlineFragment<any, any>>> // @bug are we are losing inference here since `SelectionSet<[Field<'id'>]>` works?
+  >
+  : never
   : never;
 
 export type SpreadFragment<
   Schema extends Record<string, any>,
   Fragment extends InlineFragment<any, any>,
-  CommonSelection extends SelectionSet<ReadonlyArray<Field<any, any, any>>>
+  CommonSelection extends SelectionSet<ReadonlyArray<Field<any, any, any>>>,
 > = Fragment extends InlineFragment<
   NamedType<infer Typename>,
   infer SelectionSet
+> ? Merge<
+  { __typename: Typename },
+  Result<
+    Schema,
+    Schema[Typename],
+    MergeSelectionSets<SelectionSet, CommonSelection>
+  >
 >
-  ? Merge<
-      { __typename: Typename },
-      Result<
-        Schema,
-        Schema[Typename],
-        MergeSelectionSets<SelectionSet, CommonSelection>
-      >
-    >
   : never;
 
 export type MergeSelectionSets<
   A extends SelectionSet<ReadonlyArray<Selection>>,
-  B extends SelectionSet<ReadonlyArray<Selection>>
+  B extends SelectionSet<ReadonlyArray<Selection>>,
 > = SelectionSet<L.Concat<A["selections"], B["selections"]>>;
 
-type HasInlineFragment<T extends SelectionSet<any> | undefined> =
-  T extends SelectionSet<infer Selections>
-    ? L.Includes<Selections, InlineFragment<any, any>>
-    : never;
+type HasInlineFragment<T extends SelectionSet<any> | undefined> = T extends SelectionSet<infer Selections>
+  ? L.Includes<Selections, InlineFragment<any, any>>
+  : never;
 
 type Merge<M, N> = Omit<M, keyof N> & N;
